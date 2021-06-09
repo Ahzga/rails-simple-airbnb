19f3a5650881df3350fdbf3e1c8ee2065d055d1e
Gaspard Zha <gaspard.zha@gmail.com>
2021-06-09 17:03:10 +0200

    tried some code, bootstrap, and more generally css is still difficult to manage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Not light nor Spacious Flat Paris',
+  address: '10 place de la République',
+  description: 'Un appart sans charme, sans luminosité, dont le seul avantage est la localisation',
+  price_per_night: 15,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Charmant duplex',
+  address: '24bis Place de la Nation',
+  description: 'Sympa appart',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Charmant quadruplex',
+  address: '10 place de lEtoile',
+  description: 'Superbe appartement proche de toutes commodités',
+  price_per_night: 60,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Second Light & Spacious Garden Flat London',
+  address: '100 Clifton Gardens London W9 1DT',
+  description: 'Second A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 6
+)
